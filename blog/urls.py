c06ab20d72dec925e362cda1a1d7a2d01f7bd5a6
ezaguidark <ezaguidavid@gmail.com>
2023-06-15 20:42:21 -0400

    New Post Template

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path # esto va basicamente en todos los urls.py
-from .views import BlogListView, BlogDetailView # aqui van todas las views de la app
+from .views import BlogListView, BlogDetailView, BlogCreateView # aqui van todas las views de la app
 
 urlpatterns = [
     path('', BlogListView.as_view(), name= 'home'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
 ]
