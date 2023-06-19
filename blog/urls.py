a7a2cc9f99a8e646ff67c861a4cb61098172d8ab
ezaguidark <ezaguidavid@gmail.com>
2023-06-19 00:09:09 -0400

    Adding Delete Post Funtion

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path # esto va basicamente en todos los urls.py
-from .views import BlogListView, BlogDetailView, BlogCreateView # aqui van todas las views de la app
+from .views import (
+    BlogListView,
+    BlogDetailView, 
+    BlogCreateView, 
+    BlogUpdateView,
+    BlogDeleteView,
+ ) # aqui van todas las views de la app
 
 urlpatterns = [
     path('', BlogListView.as_view(), name= 'home'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit', BlogUpdateView.as_view(), name = 'post_edit'),
+    path('post/<int:pk>/delete', BlogDeleteView.as_view(), name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView
+# Por alguna razon, las clases de abajo tambien las podia importar directamente de django.views.generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 # Create your views here.
@@ -18,3 +21,13 @@ class BlogCreateView(CreateView):
     template_name = 'post_new.html'
     # Los parametros en fields deben tener los mismos nombres que en Models.py
     fields = ['title', 'author', 'body', 'img'] 
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body', 'img']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
